ce2117917fe308143d95fa3c2c40a1f8bc8c6ec0
David Caro <carogallegodavid@gmail.com>
2023-02-23 18:38:22 +0100

    feat: install eslint, husky and commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
   extends: ['mantine'],
+  plugins: ['import'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
   parserOptions: {
     project: './tsconfig.json',
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'error',
   },
 };
